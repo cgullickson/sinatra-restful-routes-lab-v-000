2c5f0d5d81d8af551226bdbcf0d7a0c2664d1126
Chris Gullickson <cgullickson5@gmail.com>
2018-01-19 00:14:36 +0000

    automatically backed up by learn

--- db/migrate/20180119001025_create_recipes.rb
+++ db/migrate/20180119001025_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def up
     create_table :recipes do |t|
       t.string :name
-      t.string :name
+      t.string :ingredients
       t.string :cook_time
     end
   end
